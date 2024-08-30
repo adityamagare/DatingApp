494c7d4c27d73cdec0df1e84d560ba6095c7584c
Aditya Magare <aditya.magare@veritas.com>
2024-08-30 16:42:39 +0530

    End of section2

---
+++ API/Program.cs
@@ -0,0 +1,65 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//access the configuration directly from builder
+var configuration = builder.Configuration;
+var myConnectionString =  configuration.GetConnectionString("DefaultConnection");
+
+//add dbcontext to all part of code so we ingested it as service
+builder.Services.AddDbContext<DataContext>(options =>{
+
+    options.UseSqlite(myConnectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseRouting();
+
+
+// Map controllers to endpoints
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
